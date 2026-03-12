7244715b505b3bad89f42a579e477034060e393b
moqaddashusain71-rgb <moqaddashusain71@gmail.com>
2026-03-12 16:54:37 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,24 +1,27 @@
-const NEWS_API="https://newsapi.org/v2/everything?q=cricket&apiKey=00279dc951144e43b8413ac3ce36137e";
+const NEWS="https://newsapi.org/v2/everything?q=cricket&apiKey=00279dc951144e43b8413ac3ce36137e";
 
 async function loadNews(){
 
-const res=await fetch(NEWS_API);
+const res=await fetch(NEWS);
 const data=await res.json();
 
 const container=document.getElementById("news");
 
-container.innerHTML="";
+data.articles.slice(0,10).forEach(n=>{
 
-data.articles.slice(0,5).forEach(article=>{
+container.innerHTML+=`
 
-const div=document.createElement("div");
+<div class="card">
 
-div.innerHTML=`
-<h4>${article.title}</h4>
-<a href="${article.url}" target="_blank">Read</a>
+<h3>${n.title}</h3>
+
+<p>${n.source.name}</p>
+
+<a href="${n.url}" target="_blank">Read More</a>
+
+</div>
+
 `;
-
-container.appendChild(div);
 
 });
 
